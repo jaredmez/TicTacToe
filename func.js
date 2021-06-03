e1bc6b163689fbbc1140fba8338db5b93ad4a62f
Jared Mezquita <mezquitaj@gmail.com>
2021-06-03 18:18:21 -0400

    GameControl is working

--- func.js
+++ func.js
@@ -1,6 +1,6 @@
 
 const gameBoard = (() => {
-    let board = ['O','X','X','','O','X','','',''];
+    let board = ['','','','','','','','',''];
 
     const push = (position, player) => {
         if (player == 'X') {
@@ -29,21 +29,40 @@ const player = (pname,choice) => {
     return {pname, choice}
 }
 
-/* const gameControl = (() => {
-    const start = ()
-}
-)(); */
-
-
-const display = () => {
-gridItems = document.querySelector('.grid-container').getElementsByTagName("div");
-
-for (i = 0; i < 9; i++) {
-    if (!gameBoard.isBlank(i)) {
-        gridItems[i].innerText = gameBoard.board[i];
+const gameControl = (() => {
+    let count = 0;
+    
+    const play = () => {
+        if (count % 2 == 0) {
+            count++;
+            return 'X'
+        }
+        else count++;
+            return 'O';
     }
+    return {play};
 }
-}
+)();
 
-display();
+
+// const display = () => {
+// gridItems = document.querySelector('.grid-container').getElementsByTagName("button");
+
+// for (i = 0; i < 9; i++) {
+//     if (!gameBoard.isBlank(i)) {
+//         gridItems[i].innerText = gameBoard.board[i];
+//     }
+// }
+// }
+
+gridItems = Array.from(document.querySelector('.grid-container').getElementsByTagName("button"));
+console.log(gridItems);
+gridItems.forEach(item => item.addEventListener('click', markBoard));
+
+function markBoard(e) { 
+    let currentMark = gameControl.play();
+    let gridLocation= e.target.className;
+    gameBoard.push(gridLocation-1, currentMark);
+    console.log(currentMark);
+};
 
