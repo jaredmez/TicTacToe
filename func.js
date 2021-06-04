41a7f21377959d587909f3a0521b72111301c3d2
Jared Mezquita <mezquitaj@gmail.com>
2021-06-04 17:17:56 -0400

    Added who Won feature

--- func.js
+++ func.js
@@ -13,6 +13,33 @@ const gameBoard = (() => {
 
     const showBoard = () => console.log(board);
 
+    const check = () => {
+        if (board[0] != '' && board[0]===board[1] && board[0] === board[2]) {
+            console.log('we have a winner')
+        }
+        else if (board[3] != '' && board[3]===board[4] && board[3] === board[5]) {
+            console.log('we have a winner')
+        }
+        else if (board[6] != '' && board[6]===board[7] && board[6] === board[8]) {
+            console.log('we have a winner')
+        }
+        else if (board[0] != '' && board[0]===board[3] && board[0] === board[6]) {
+            console.log('we have a winner')
+        }
+        else if (board[1] != '' && board[1]===board[4] && board[1] === board[7]) {
+            console.log('we have a winner')
+        }
+        else if (board[2] != '' && board[2]===board[5] && board[2] === board[8]) {
+            console.log('we have a winner')
+        }
+        else if (board[0] != '' && board[0]===board[4] && board[0] === board[8]) {
+            console.log('we have a winner')
+        }
+        else if (board[2] != '' && board[2]===board[4] && board[2] === board[6]) {
+            console.log('we have a winner')
+        }
+    };
+
     const isBlank = (index) => {
         if (board[index] == '') {
             return true
@@ -20,7 +47,7 @@ const gameBoard = (() => {
         else return false;
     }
 
-    return {board, push, showBoard, isBlank};
+    return {board, push, showBoard, check, isBlank};
 }
 )();
 
@@ -44,25 +71,22 @@ const gameControl = (() => {
 }
 )();
 
-
-// const display = () => {
-// gridItems = document.querySelector('.grid-container').getElementsByTagName("button");
-
-// for (i = 0; i < 9; i++) {
-//     if (!gameBoard.isBlank(i)) {
-//         gridItems[i].innerText = gameBoard.board[i];
-//     }
-// }
-// }
-
+//add onclick event to all grid squares
 gridItems = Array.from(document.querySelector('.grid-container').getElementsByTagName("button"));
-console.log(gridItems);
 gridItems.forEach(item => item.addEventListener('click', markBoard));
 
+
+//square onclick handler
 function markBoard(e) { 
     let currentMark = gameControl.play();
     let gridLocation= e.target.className;
+    gridItems.forEach(item => {
+        if (item.className === gridLocation) {
+            item.innerHTML = currentMark;
+            item.removeEventListener("click", markBoard);
+        }
+    })
     gameBoard.push(gridLocation-1, currentMark);
+    gameBoard.check();
     console.log(currentMark);
 };
-
